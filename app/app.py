afed676bef96838702a89eab6f5a85e31d3e5b53
boraj <test>
2025-12-30 10:45:03 +0100

    app.py creation

---
+++ app/app.py
@@ -0,0 +1,51 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import boto3
+from botocore.exceptions import ClientError
+import os
+
+# App initialization
+app = FastAPI(title="DevSecOps Lab API")
+
+# The entity DynaoDB Table
+dynamodb = boto3.resource('dynamodb', region_name='eu-west-3')
+
+# The table name I created
+table_name = "devsecops-items" # Must match with Terraform
+
+# The table in AWS
+table = dynamodb.Table(table_name)
+
+## Pydantic Basic Validation ##
+class Item(BaseModel):
+    id: str
+    name: str
+    description: str = None
+
+
+# -- Endpoints -- 
+
+@app.get("/")
+def read_root():
+    return {"status": "ok", "message": "DevSecOps Lab is Running!"}
+
+@app.post("/items/")
+def create_item(item: Item):
+    """Stores an item in the DynamoDB Table"""
+    try:
+        table.put_item(Item=item.dict())
+        return {"message": "Item stored successfully", "item": item}
+    except ClientError as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@app.get("/items/{item_id}")
+def read_item(item_id: str):
+    """Read an item from the DynamoDB Table"""
+    try:
+        response = table.get_item(Key={'id': item_id})
+        item = response.get('Item')
+        if not item:
+            raise HTTPException(status_code=404, detail="Item no encontrado")
+        return item
+    except ClientError as e:
+        raise HTTPException(status_code=500, detail=str(e))
